2a16b9b1fc357ad73243a45d0bfbd671e10c2476
MirkoArtem <aikmir64@gmail.com>
2024-02-11 19:15:01 +0300

    add Lesson_10

---
+++ src/main/java/Lesson_10/Fruit.java
@@ -0,0 +1,12 @@
+package Lesson_10;
+
+public class Fruit {
+    protected float weight;
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+    public float getWeight(){
+        return weight;
+    }
+}
---
+++ src/main/java/Lesson_10/Orange.java
@@ -0,0 +1,9 @@
+package Lesson_10;
+
+import Lesson_10.Fruit;
+
+public class Orange extends Fruit {
+    public Orange(){
+        super(1.5f);
+    }
+}
