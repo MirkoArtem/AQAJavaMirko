11de039603c72c7ad84a3d1d7e72f26ffcb14567
MirkoArtem <141056857+MirkoArtem@users.noreply.github.com>
2024-02-11 17:52:54 +0300

    Delete src/main/java/Lesson_7 directory

--- src/main/java/Lesson_7/MyArrayDataException.java
+++
@@ -1,7 +0,0 @@
-package Lesson_7;
-
-public class MyArrayDataException extends MyCustomExceptions{
-    public MyArrayDataException(int row, int col) {
-        super(String.format("Неверные данные находятся в ячейке [%d, %d]\n", row, col));
-    }
-}
