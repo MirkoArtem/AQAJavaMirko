c9b4fc3b15c902daf13840ca1d1ed5cea34a00c3
MirkoArtem <aikmir64@gmail.com>
2024-02-11 17:48:37 +0300

    add Lesson_9

---
+++ src/main/java/Lesson_9/Task_4/Student.java
@@ -0,0 +1,5 @@
+package Lesson_9.Task_4;
+
+public interface Student {
+    int HashCode();
+}
---
+++ src/main/java/Lesson_9/Task_5/Main.java
@@ -0,0 +1,7 @@
+package Lesson_9.Task_5;
+
+public class Main {
+    public static void main(String[] args) {
+        Logins.loginsFromKeyboard();
+    }
+}
