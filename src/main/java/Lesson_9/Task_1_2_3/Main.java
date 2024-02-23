1cae4b12493f5bb0077e64817bec57f6083927fa
MirkoArtem <141056857+MirkoArtem@users.noreply.github.com>
2024-02-23 19:40:36 +0300

    Delete src/main/java/Lesson_9 directory

--- src/main/java/Lesson_9/Task_1_2_3/Main.java
+++
@@ -1,9 +0,0 @@
-package Lesson_9.Task_1_2_3;
-
-public class Main {
-    public static void main(String[] args) {
-        RandomNumbers.randomNumbers();
-        StringCollection.stringCollection();
-        ElementsCollection.elementsCollection();
-    }
-}
