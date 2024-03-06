eca5f49cfb0755378c836306767b241fe810be83
MirkoArtem <141056857+MirkoArtem@users.noreply.github.com>
2024-03-06 16:12:31 +0300

    Delete src/main/java directory

--- src/main/java/Lesson_12_junit_5/Main.java
+++
@@ -1,7 +0,0 @@
-package Lesson_12_junit_5;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(Factorial.calculateFactorial(5));
-    }
-}
--- src/main/java/Lesson_9/Task_4/Student.java
+++
@@ -1,5 +0,0 @@
-package Lesson_9.Task_4;
-
-public interface Student {
-    int HashCode();
-}
